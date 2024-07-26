acfdc4cb36ad868dd121e457356b1e8bdc20a592
Bhargav-2001 <107164552+Bhargav-2001@users.noreply.github.com>
2024-07-26 13:38:34 +0530

    first6

--- src/test/java/com/example/demo/AuthControllerTest.java
+++ src/test/java/com/example/demo/AuthControllerTest.java
@@ -1,22 +1,6 @@
 package com.example.demo;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.Matchers.is;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthControllerTest {
 }
