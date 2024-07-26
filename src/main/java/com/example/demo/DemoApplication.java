c8d8260413ef50320a215e5c558587a3b666691b
Bhargav-2001 <107164552+Bhargav-2001@users.noreply.github.com>
2024-07-26 14:39:22 +0530

    Hello

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,17 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @OpenAPIDefinition(info = @Info(title = "Patient Connect APIS"))
 public class DemoApplication {
+	@GetMapping("/message")
+	public String message(){
+		return "hiiiii";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
